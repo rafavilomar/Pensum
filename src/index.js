a46a187e3049051074c9c0e9a23a807ecc087e6f
Rafael Vilomar <rafavilomar@gmail.com>
2021-08-18 11:47:15 -0400

    handling error to read file.json

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import {read, write} from './modules/File.js'
 
 const main = async () => {
   let temp = await read();
-    
-  let temp2 = [{
-    "name": "name"
-  }] 
-  write(temp2);
 
-  console.table(temp);
-  console.table(temp2);
+  console.log(temp);
 }
 
 
--- src/modules/File.js
+++ src/modules/File.js
@@ -2,18 +2,28 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+// ----------
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const PATH = `${__dirname}/../file.json`
+
+// ----------
 
 export const read = () => {
   console.log(PATH);
   return new Promise((resolve, reject) => {
     fs.readFile(PATH, (err, data) => {
       if (err) {
-        reject(err)
+        write([])
+      }else{
+        try {
+          resolve(JSON.parse(data))
+        } catch (error) {
+          console.error('Something was wrong with FILE.JSON. We are working on it...');
+          write([])
+        }
       }
-      resolve(JSON.parse(data))
     })
   })
 }
@@ -23,8 +33,9 @@ export const write = (data) => {
     fs.writeFile(PATH, JSON.stringify(data), (err) => {
       if (err) {
         reject(err)
+      }else {
+        resolve('Done!')
       }
-      resolve(data)
     })
   })
 }
