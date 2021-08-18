26f999c276f7fbc90346c317c6d14b3d224647f4
Rafael Vilomar <rafavilomar@gmail.com>
2021-08-18 10:53:35 -0400

    1rst commit

---
+++ src/modules/File.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const PATH = `${__dirname}/../file.json`
+
+export const read = () => {
+  console.log(PATH);
+  return new Promise((resolve, reject) => {
+    fs.readFile(PATH, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(JSON.parse(data))
+    })
+  })
+}
+
+export const write = (data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(PATH, JSON.stringify(data), (err) => {
+      if (err) {
+        reject(err)
+      }
+      //resolve(JSON.parse(data))
+    })
+    //  err ? reject(err) : resolve(JSON.parse(data))})
+  })
+}
+
+//module.exports = {
+//  read,
+//  write
+//};
